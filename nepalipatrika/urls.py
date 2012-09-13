b4851b3f1b4e3f3f27da134f8ddbe6a2fb01fbc9
Bibek Shrestha Hybris <bibek.shrestha@hybris.com>
2012-09-13 02:39:04 +0200

    Removed admin urls

--- nepalipatrika/urls.py
+++ nepalipatrika/urls.py
@@ -33,7 +33,7 @@ urlpatterns += patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
 
 #handler500 = 'fileindex.views.custom_500'
